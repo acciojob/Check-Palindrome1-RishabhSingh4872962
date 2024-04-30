2b9379de9be750551a953ac7fa00740bab28ad15
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-04-30 14:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
-
+	
+str=str.split(" ").filter((s)=>{
+	return !(s==" " || s=="");
+}).join("").toLowerCase();
+	if (str.length==1) {
+		return true;
+	}
+	let i=0;
+	let j=str.length-1;
+	while (i<j) {
+		if (str.charAt(i) != str.charAt(j)) {
+			
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true
 }
 module.exports = palindrome
